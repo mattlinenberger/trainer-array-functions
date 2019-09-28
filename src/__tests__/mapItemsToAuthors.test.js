19279cf01b52e16c4914b9ef371c2f689f7fa418
Matt Linenberger <mattlinenberger@gmail.com>
2019-09-28 17:21:05 -0500

    practiced arrowFunctions/unitTests/destructorting/iterables

--- src/__tests__/mapItemsToAuthors.test.js
+++ src/__tests__/mapItemsToAuthors.test.js
@@ -1,5 +1,8 @@
 import {
     mapItemsToAuthors,
+    filterItemsByUps,
+    calculateTotalScores,
+    getAllCategories,
 } from '../mapItemsToAuthors';
 
 /* import data for testing */
@@ -11,6 +14,37 @@ const numberOfListItems = redditScienceListing.data.children.length;
 /**
  * Unit tests
  */
+
+describe('calculateTotalScores', () => {
+
+    it('Should return results > 0', () => {
+        const result = calculateTotalScores();
+        console.log(`total score is: ${result}`);
+        expect(result)
+            .toBeGreaterThan(0);
+    });
+});
+describe('getAllCategories', () => {
+
+    it('Should return a array of categories > 0', () => {
+        const result = getAllCategories();
+        console.log(`categories are: ${result}`);
+        expect(result.length)
+            .toBeGreaterThan(0);
+    });
+});
+
+describe('filterItemsByUps', () => {
+
+    it('Ups should return items', () => {
+        const result = filterItemsByUps();
+
+        expect(result.length)
+            .toBeGreaterThan(0);
+    });
+
+});
+
 describe('mapItemsToAuthors() using test data:', () => {
     const result = mapItemsToAuthors();
 
--- src/mapItemsToAuthors.js
+++ src/mapItemsToAuthors.js
@@ -20,8 +20,51 @@ const items = redditScienceListing.data.children;
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
  */
 export const mapItemsToAuthors = () => {
-    
+    return items.map((currentItem) => {
+        return currentItem.data.author;
+    });
 };
+
+export const getAllCategories = () => {
+
+    return items.filter((currentItem) => {
+        return currentItem.data.category !== null;
+    }).map((currentItem) => {
+        const {
+            category
+        } = currentItem.data;
+        return category;
+    })
+};
+
+export const filterItemsByUps = () => {
+    const result = items
+        //filter returns [] from the condition given.
+        .filter((currentItem) => {
+            return currentItem.data.ups >= 10 && currentItem.data.ups < 100;
+        })
+        //map returns a new [].
+        .map((currentItem) => {
+            return {
+                ups: currentItem.data.ups,
+                author: currentItem.data.author
+            }
+        });
+    console.log(result);
+    return result
+}
+
+export const calculateTotalScores = () => {
+    return items.reduce((acc, currentItem) => {
+
+        const {
+            score: currentScore,
+        } = currentItem.data;
+
+        console.log(`currentScore: ${currentScore}, totalScore: ${acc}`);
+        return acc + currentScore;
+    }, 0);
+}
 
 export default {
     mapItemsToAuthors,
